3e9d4d8c496bb727a1ec9f0bbe87f1da50c0eea5
Min901128 <mike0975400248@gmail.com>
2023-05-09 17:02:17 +0800

    Passport Version1

---
+++ Assets/Script/MakePassport.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MakePassport : MonoBehaviour
+{
+    public Text Name;
+    public Text Nationality;
+    public Text Sex;
+    public Text BirthPlace;
+    public Text BirthDay;
+
+    public Text YourName;
+    public Text YourNationality;
+    public Text YourSex;
+    public Text YourBirthPlace;
+    public Text YourBirthDay;
+    
+    public void ShowData(){
+        YourName.text = Name.text;
+        YourNationality.text = Nationality.text;
+        YourSex.text = Sex.text;
+        YourBirthPlace.text = BirthPlace.text;
+        YourBirthDay.text = BirthDay.text;
+    }
+}

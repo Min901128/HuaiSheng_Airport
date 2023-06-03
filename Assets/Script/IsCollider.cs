05de9fd305f4be1308ada5fff31340a141773627
Min901128 <mike0975400248@gmail.com>
2023-06-03 16:17:19 +0800

    違禁品分辨功能完成

--- Assets/Script/IsCollider.cs
+++ Assets/Script/IsCollider.cs
@@ -3,19 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class IsCollider : MonoBehaviour{
     public GameObject item;
+    public Text CScore;
     private bool IsHasCollider = false;
-    
-    private void OnTriggerStay(Collider other){   
-        //Debug.Log("碰到" + other.tag);
-        if(item.tag == "Yes" && other.tag == "Yes" && Input.GetMouseButtonUp(0) && IsHasCollider == false){
-            item.SetActive(false);
-            IsHasCollider = true;
-        }
+    private int CorrectScore;
+    public UnityEvent CallAddScore;
 
-        if(item.tag == "No" && other.tag == "No" && Input.GetMouseButtonUp(0) && IsHasCollider == false){
+    private void OnTriggerStay(Collider other){
+        //Debug.Log("碰到" + other.tag);
+        if(Input.GetMouseButtonUp(0) && IsHasCollider == false){ 
+           if(item.tag == "Yes" && other.tag == "Yes"){
+                CallAddScore.Invoke();
+           } 
+           if(item.tag == "No" && other.tag == "No"){
+                CallAddScore.Invoke();
+            }
             item.SetActive(false);
             IsHasCollider = true;
         }

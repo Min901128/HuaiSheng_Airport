e5cd50eadb66eca1cbb9c13f501df8d07238874f
wannaflyhigh <joeydai901108@gmail.com>
2023-06-14 01:31:09 +0800

    修改小遊戲場景：新增倒數、提示框

--- Assets/Script/Game_Handler.cs
+++ Assets/Script/Game_Handler.cs
@@ -6,21 +6,46 @@ using UnityEngine.UI;
 public class Game_Handler : MonoBehaviour
 {
 	public Text Score;
+	public Text count_down_text;
 	public GameObject target_sample;
 	public GameObject canvas;
+	public GameObject Game_Start_Obj;
+	public GameObject Game_End_Obj;
 	int Score_Points=0;
+	float count_down=30;
+	bool is_started=false;
+	bool is_ended=false;
     // Start is called before the first frame update
     void Start()
     {
 		Score.text=Score_Points.ToString();
-		createNewTarget();
+		count_down_text.text=count_down.ToString();
+		Game_Start_Obj.SetActive(true);
+		Game_End_Obj.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+		if(is_started==false)return;
+		if(is_ended==true)return;
+		if(count_down<0){
+			is_ended=true;
+			End_Game();
+		}
+		count_down-=Time.deltaTime;
+		count_down_text.text=Mathf.Ceil(count_down).ToString();
     }
+
+	public void Game_Start(){
+		Game_Start_Obj.SetActive(false);
+		is_started=true;
+		createNewTarget();
+	}
+
+	public void End_Game(){
+		Game_End_Obj.SetActive(true);
+	}
 
 	public void createNewTarget()
 	{

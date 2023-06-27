6965405ee1644e97378b8a78bff3dacb3506db2d
wannaflyhigh <joeydai901108@gmail.com>
2023-06-28 06:02:49 +0800

    修正ar路線問題，更改圖示

--- Assets/Script/AR_StateHandler.cs
+++ Assets/Script/AR_StateHandler.cs
@@ -11,23 +11,28 @@ public class AR_StateHandler : MonoBehaviour
 	public string[] scene_names;
 	float hint_countdown;
 	bool counting=false;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		// SetFinishedARState(0);
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
+	// Update is called once per frame
+	void Update()
+	{
 		if(counting==false)return;
-        hint_countdown-=Time.deltaTime;
+		hint_countdown-=Time.deltaTime;
 		if(hint_countdown<0){
 			counting=false;
 			passed.SetActive(false);
 			not_yet.SetActive(false);
 		}
-    }
+	}
+
+	public void SetFinishedARState(int state){
+		PlayerPrefs.SetInt("finished_state",state);
+		PlayerPrefs.Save();
+	}
 
 	int map_playerperf_index_to_scence_index(int playerperf_index){
 		if(playerperf_index==10)return 0;
@@ -46,19 +51,47 @@ public class AR_StateHandler : MonoBehaviour
 
 	public void onScanned(int value_scanned){
 		Debug.Log(value_scanned);
-		int next_state=PlayerPrefs.GetInt("next_state");
-		Debug.Log(next_state);
-		if(value_scanned==next_state){
-			// goto specific scene
-			SceneManager.LoadScene(scene_names[map_playerperf_index_to_scence_index(next_state)], LoadSceneMode.Single);
+		int finished_state=PlayerPrefs.GetInt("finished_state",0);
+		Debug.Log(finished_state);
+		if(finished_state==60 && value_scanned/10==6)
+		{
+			SceneManager.LoadScene(scene_names[map_playerperf_index_to_scence_index(value_scanned)]);
 		}
-		if(value_scanned>next_state)
+		if(finished_state==60)
+		{
+			string P1Key = "P1";
+			string P2Key = "P2";
+			string P3Key = "P3";
+			string P4Key = "P4";
+			int P1Index = PlayerPrefs.GetInt(P1Key); 
+			int P2Index = PlayerPrefs.GetInt(P2Key);
+			int P3Index = PlayerPrefs.GetInt(P3Key);
+			int P4Index = PlayerPrefs.GetInt(P4Key);
+			if(P1Index==1 && P2Index==1 && P3Index==1 && P4Index==1){
+				SceneManager.LoadScene(scene_names[map_playerperf_index_to_scence_index(value_scanned)]);
+			}else{
+				hint_countdown=3f;
+				counting=true;
+				not_yet.SetActive(true);
+			}
+			return;
+		}
+		if(finished_state==50 && value_scanned/10==6)
+		{
+			SceneManager.LoadScene(scene_names[map_playerperf_index_to_scence_index(value_scanned)]);
+		}
+
+		if(value_scanned==finished_state+10)
+		{
+			SceneManager.LoadScene(scene_names[map_playerperf_index_to_scence_index(value_scanned)]);
+		}
+		if(value_scanned>finished_state+10)
 		{
 			hint_countdown=3f;
 			counting=true;
 			not_yet.SetActive(true);
 		}
-		if(value_scanned<next_state)
+		if(value_scanned<finished_state+10)
 		{
 			hint_countdown=3f;
 			counting=true;

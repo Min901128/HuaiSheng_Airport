8a133312b7743f5898bdfd3b592f7382b540c52f
wannaflyhigh <joeydai901108@gmail.com>
2024-11-04 01:36:51 +0800

    修復大頭照不會被清除

--- Assets/Script/ClearRecord.cs
+++ Assets/Script/ClearRecord.cs
@@ -1,10 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class ClearRecord : MonoBehaviour
 {
 	public void Clear(){
 		PlayerPrefs.DeleteAll();
+		DeleteFile("selfie.png");
+	}
+
+	public void DeleteFile(string fileName)
+	{
+		// Construct the full path of the file
+		string filePath = Path.Combine(Application.persistentDataPath, fileName);
+
+		// Check if the file exists before attempting to delete
+		if (File.Exists(filePath))
+		{
+			File.Delete(filePath);
+			Debug.Log($"File {fileName} deleted successfully.");
+		}
+		else
+		{
+			Debug.LogWarning($"File {fileName} does not exist at {filePath}.");
+		}
 	}
 }

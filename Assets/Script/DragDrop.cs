17648dc1a67ca3d79d2954144ce1b355bba48445
wannaflyhigh <joeydai901108@gmail.com>
2024-11-06 21:29:14 +0800

    修復拼圖到位後依然會被拖動

--- Assets/Script/DragDrop.cs
+++ Assets/Script/DragDrop.cs
@@ -6,21 +6,37 @@ using UnityEngine.EventSystems;
 public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
     private RectTransform rectTransform;
+    private bool IsDraggable = true;
 
-    private void Awake(){
+
+    private void Awake()
+    {
         rectTransform = GetComponent<RectTransform>();
     }
-    public void OnBeginDrag(PointerEventData eventData){
+    public void OnBeginDrag(PointerEventData eventData)
+    {
         //Debug.Log("OnBeginDrag");
     }
-    public void OnDrag(PointerEventData eventData){
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (!IsDraggable)
+        {
+            return;
+        }
         //Debug.Log("OnBeginDrag");
         rectTransform.anchoredPosition += eventData.delta;
     }
-    public void OnEndDrag(PointerEventData eventData){
+    public void OnEndDrag(PointerEventData eventData)
+    {
         //Debug.Log("OnEndDrag");
     }
     public void OnPointerDown(PointerEventData eventData){
         //Debug.Log("OnPointerDown");
+    }
+
+    public void DisableDragDrop()
+    {
+        IsDraggable = false;
     }
 }
